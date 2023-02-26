21526e5aee2824592f2c35b4a459c81b2d205317
Qwdsss <artemiy.bobrus@gmail.com>
2023-02-26 23:33:54 +0600

    cp3

---
+++ CP3/todo_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class TodoList(models.Model):
+    DoesNotExist = None
+    objects = None
+    name = models.CharField(max_length=255, null=False)
+
+
+    class Meta:
+        verbose_name = 'TodoList'
+        verbose_name_plural = 'TodoLists'
+
+    def __str__(self):
+        return 'ID: {}, Name: {}'.format(self.id, self.name)
+
+
+class Todo(models.Model):
+    DoesNotExist = None
+    objects = None
+    name = models.CharField(max_length=255, null=False)
+    done = models.BooleanField(default=False, null=False)
+    todo_list = models.ForeignKey(TodoList, null=False, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Todo'
+        verbose_name_plural = 'Todos'
+
+    def __str__(self):
+        return 'ID: {}, Name: {}'.format(self.id, self.name)
